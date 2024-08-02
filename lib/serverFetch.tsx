63ec46642eccf00c4397b716ecb38484dac24c05
hamster1963 <1410514192@qq.com>
2024-08-02 11:58:47 +0800

    fix: data repetition

--- lib/serverFetch.tsx
+++ lib/serverFetch.tsx
@@ -21,7 +21,7 @@ export async function GetNezhaData() {
         Authorization: process.env.NezhaAuth as string,
       },
       next: {
-        revalidate: 1,
+        revalidate: 0,
       },
     });
     const nezhaData = (await response.json()).result as NezhaAPI[];
