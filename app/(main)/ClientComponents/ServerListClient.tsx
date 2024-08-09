0f1649c51c75ba8a22406ff02b467edc1a04f65e
hamster1963 <1410514192@qq.com>
2024-08-09 17:21:04 +0800

    feat: add display sort

--- app/(main)/ClientComponents/ServerListClient.tsx
+++ app/(main)/ClientComponents/ServerListClient.tsx
@@ -7,16 +7,23 @@ import useSWR from "swr";
 
 export default function ServerListClient() {
   const { data } = useSWR<ServerApi>("/api/server", nezhaFetcher, {
-    refreshInterval: process.env.NEXT_PUBLIC_NezhaFetchInterval
-      ? Number(process.env.NEXT_PUBLIC_NezhaFetchInterval)
-      : 2000,
+    refreshInterval: Number(process.env.NEXT_PUBLIC_NezhaFetchInterval) || 2000,
   });
+
   if (!data) return null;
-  const sortedResult = data.result.sort((a, b) => a.id - b.id);
+
+  const sortedServers = data.result.sort((a, b) => {
+    if (a.display_index && b.display_index) {
+      return b.display_index - a.display_index;
+    }
+    if (a.display_index) return -1;
+    if (b.display_index) return 1;
+    return a.id - b.id;
+  });
 
   return (
-    <section className={"grid grid-cols-1 gap-2 md:grid-cols-2"}>
-      {sortedResult.map((serverInfo) => (
+    <section className="grid grid-cols-1 gap-2 md:grid-cols-2">
+      {sortedServers.map((serverInfo) => (
         <ServerCard key={serverInfo.id} serverInfo={serverInfo} />
       ))}
     </section>
