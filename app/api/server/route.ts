abdd7f245fcac580544bc74e3d0af072269b69fb
hamster1963 <1410514192@qq.com>
2024-09-22 21:50:26 +0800

    feat: add Docker Hub

--- app/api/server/route.ts
+++ app/api/server/route.ts
@@ -9,6 +9,9 @@ export async function GET(_: Request) {
     return NextResponse.json(response, { status: 200 });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: "fetch nezha data failed" }, { status: 400 });
+    return NextResponse.json(
+      { error: "fetch nezha data failed" },
+      { status: 400 },
+    );
   }
 }
