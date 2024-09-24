4208f16199ef2b4e0de55ab2698000c60007d2d5
hamster1963 <1410514192@qq.com>
2024-09-24 14:14:52 +0800

    feat: add cloudflare support

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,22 +1,13 @@
 import ServerList from "@/components/ServerList";
 import ServerOverview from "@/components/ServerOverview";
-import { GetNezhaData } from "@/lib/serverFetch";
 
-import { SWRConfig } from "swr";
+export const runtime = 'edge';
 
 export default function Home() {
   return (
-    <SWRConfig
-      value={{
-        fallback: {
-          "/api/server": GetNezhaData(),
-        },
-      }}
-    >
-      <div className="mx-auto grid w-full max-w-5xl gap-4 md:gap-6">
-        <ServerOverview />
-        <ServerList />
-      </div>
-    </SWRConfig>
+    <div className="mx-auto grid w-full max-w-5xl gap-4 md:gap-6">
+      <ServerOverview />
+      <ServerList />
+    </div>
   );
 }
--- app/api/server/route.ts
+++ app/api/server/route.ts
@@ -1,7 +1,11 @@
-import { GetNezhaData } from "@/lib/serverFetch";
+import { NezhaAPI, ServerApi } from "@/app/types/nezha-api";
+import { MakeOptional } from "@/app/types/utils";
+import getEnv from "@/lib/env-entry";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
+
+export const runtime = 'edge';
 
 export async function GET(_: Request) {
   try {
@@ -13,5 +17,59 @@ export async function GET(_: Request) {
       { error: "fetch nezha data failed" },
       { status: 400 },
     );
+  }
+}
+
+async function GetNezhaData() {
+
+  var nezhaBaseUrl = getEnv("NezhaBaseUrl");
+  if (!nezhaBaseUrl) {
+    console.log("NezhaBaseUrl is not set");
+    return;
+  }
+
+  // Remove trailing slash
+  if (nezhaBaseUrl[nezhaBaseUrl.length - 1] === "/") {
+    nezhaBaseUrl = nezhaBaseUrl.slice(0, -1);
+  }
+  try {
+    const response = await fetch(nezhaBaseUrl + "/api/v1/server/details", {
+      headers: {
+        Authorization: getEnv("NezhaAuth") as string,
+      },
+      next: {
+        revalidate: 0,
+      },
+    });
+    const nezhaData = (await response.json()).result as NezhaAPI[];
+    const data: ServerApi = {
+      live_servers: 0,
+      offline_servers: 0,
+      total_bandwidth: 0,
+      result: [],
+    };
+    const timestamp = Date.now() / 1000;
+    data.result = nezhaData.map(
+      (element: MakeOptional<NezhaAPI, "ipv4" | "ipv6" | "valid_ip">) => {
+        if (timestamp - element.last_active > 300) {
+          data.offline_servers += 1;
+          element.online_status = false;
+        } else {
+          data.live_servers += 1;
+          element.online_status = true;
+        }
+        data.total_bandwidth += element.status.NetOutTransfer;
+
+        delete element.ipv4;
+        delete element.ipv6;
+        delete element.valid_ip;
+
+        return element;
+      },
+    );
+
+    return data;
+  } catch (error) {
+    return error;
   }
 }
