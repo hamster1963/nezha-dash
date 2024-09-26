14b9cad8566c109d47bb6c5b581fc7edbd6bc89f
hamster1963 <1410514192@qq.com>
2024-09-26 14:23:53 +0800

    fix: disable prefetch

--- app/[locale]/(main)/page.tsx
+++ app/[locale]/(main)/page.tsx
@@ -1,31 +1,13 @@
 import ServerList from "../../../components/ServerList";
 import ServerOverview from "../../../components/ServerOverview";
-import getEnv from "../../../lib/env-entry";
-import { GetNezhaData } from "../../../lib/serverFetch";
-import { SWRConfig } from "swr";
 
 export const runtime = 'edge';
 
-
-const disablePrefetch = getEnv("ServerDisablePrefetch") === "true";
-const fallback = disablePrefetch
-  ? {}
-  : {
-    "/api/server": GetNezhaData(),
-  };
-
-
 export default function Home() {
   return (
-    <SWRConfig
-      value={{
-        fallback: fallback,
-      }}
-    >
-      <div className="mx-auto grid w-full max-w-5xl gap-4 md:gap-6">
-        <ServerOverview />
-        <ServerList />
-      </div>
-    </SWRConfig>
+    <div className="mx-auto grid w-full max-w-5xl gap-4 md:gap-6">
+      <ServerOverview />
+      <ServerList />
+    </div>
   );
 }
