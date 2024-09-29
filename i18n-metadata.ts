9d02f35522c8cc39bc80575ccc16c0467d5e0f6f
hamster1963 <1410514192@qq.com>
2024-09-29 11:28:01 +0800

    feat: add DefaultLocale config

--- i18n-metadata.ts
+++ i18n-metadata.ts
@@ -1,5 +1,7 @@
 // @auto-i18n-check. Please do not delete the line.
 
+import getEnv from "./lib/env-entry";
+
 export const localeItems = [
   { code: "en", name: "English" },
   { code: "ja", name: "日本語" },
@@ -21,4 +23,4 @@ export const localeItems = [
 ];
 
 export const locales = localeItems.map((item) => item.code);
-export const defaultLocale = "en";
+export const defaultLocale = getEnv("DefaultLocale") || "en";
--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,14 @@
 // @auto-i18n-check. Please do not delete the line.
 
 import createMiddleware from "next-intl/middleware";
-import { locales } from "./i18n-metadata";
+import { defaultLocale, locales } from "./i18n-metadata";
 
 export default createMiddleware({
   // A list of all locales that are supported
   locales: locales,
 
   // Used when no locale matches
-  defaultLocale: "en",
+  defaultLocale: defaultLocale,
 
   // 'always': This is the default, The home page will also be redirected to the default language, such as www.abc.com to www.abc.com/en
   // 'as-needed': The default page is not redirected. For example, if you open www.abc.com, it is still www.abc.com
