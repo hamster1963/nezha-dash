8afeb39239fbe365b76d17bf1c848e5a20f745cb
hamster1963 <1410514192@qq.com>
2024-10-09 13:52:55 +0800

    fix: blank page

--- app/[locale]/(main)/[id]/page.tsx
+++ app/[locale]/(main)/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { NetworkChartClient } from "../ClientComponents/NetworkChart";
 
+export const dynamic = "auto";
+
 export default function Page({ params }: { params: { id: string } }) {
   return (
     <div className="mx-auto grid w-full max-w-5xl gap-4 md:gap-6">
