d4e1815234871ab307902187f8a9d24b4dcaaec7
hamster1963 <1410514192@qq.com>
2024-10-09 14:11:32 +0800

    fix: vercel dynamic

--- app/[locale]/(main)/[id]/page.tsx
+++ app/[locale]/(main)/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { NetworkChartClient } from "../ClientComponents/NetworkChart";
 
 export const dynamic = "force-dynamic"
+export const revalidate = 0
 
 export default function Page({ params }: { params: { id: string } }) {
   return (
