5ef444f8183603fbea9c24bdeebac25e4900bdc8
hamster1963 <1410514192@qq.com>
2024-10-12 22:23:55 +0800

    style: ChartTooltip

--- lib/utils.ts
+++ lib/utils.ts
@@ -103,7 +103,7 @@ export function formatTime(timestamp: number): string {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  const hours = date.getHours();
+  const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
   const seconds = date.getSeconds().toString().padStart(2, "0");
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
