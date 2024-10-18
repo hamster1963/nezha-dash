7187a6ff25882c05d0542ce27306d8f38565d360
hamster1963 <1410514192@qq.com>
2024-10-18 18:01:32 +0800

    feat: init cpu chart

--- app/[locale]/(main)/detail/[id]/page.tsx
+++ app/[locale]/(main)/detail/[id]/page.tsx
@@ -1,5 +1,10 @@
 import ServerDetailClient from "@/app/[locale]/(main)/ClientComponents/ServerDetailClient";
+import ServerDetailChartClient from "@/app/[locale]/(main)/ClientComponents/ServerDetailChartClient";
 
 export default function Page({ params }: { params: { id: string } }) {
-  return <ServerDetailClient server_id={Number(params.id)} />;
+  return <div className="mx-auto grid w-full max-w-5xl gap-2">
+    <ServerDetailClient server_id={Number(params.id)} />
+    <ServerDetailChartClient server_id={Number(params.id)} />
+  </div>
+    ;
 }
