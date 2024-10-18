8e3afb86d3653978c881a88b2c2fb68ccc91c7cc
hamster1963 <1410514192@qq.com>
2024-10-18 09:53:00 +0800

    feat: detail page init

--- app/[locale]/(main)/detail/[id]/page.tsx
+++ app/[locale]/(main)/detail/[id]/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { BackIcon } from "@/components/Icon";
+import { useLocale } from "next-intl";
+import { useRouter } from "next/navigation";
+
 export default function Page({ params }: { params: { id: string } }) {
+
+  const router = useRouter();
+  const locale = useLocale();
+
   return (
     <div className="mx-auto grid w-full max-w-5xl gap-4 md:gap-6">
-      {params.id}
+      <div
+        onClick={() => {
+          router.push(`/${locale}/`);
+        }}
+        className="flex flex-none cursor-pointer font-medium items-center break-all tracking-tight gap-0.5 text-2xl"
+      >
+        <BackIcon />
+        HomeDash
+      </div>
     </div>
   );
 }
