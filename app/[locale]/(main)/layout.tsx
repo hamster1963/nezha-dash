9ad5c1f2895f800133ad006e017d928f0ee69a9f
hamster1963 <1410514192@qq.com>
2024-10-21 10:32:12 +0800

    refactor: auth component

--- app/[locale]/(main)/layout.tsx
+++ app/[locale]/(main)/layout.tsx
@@ -11,15 +11,11 @@ type DashboardProps = {
 export default async function MainLayout({ children }: DashboardProps) {
   const session = await auth();
 
-  if (!session && getEnv("SitePassword")) {
-    return <SignIn />;
-  }
-
   return (
     <div className="flex min-h-screen w-full flex-col">
       <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:p-10 md:pt-8">
         <Header />
-        {children}
+        {!session && getEnv("SitePassword") ? <SignIn /> : children}
         <Footer />
       </main>
     </div>
