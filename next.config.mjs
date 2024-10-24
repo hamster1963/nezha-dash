ae844d103164461389104374d5549ee8c1abf229
hamster1963 <1410514192@qq.com>
2024-10-24 21:15:53 +0800

    fix: docker

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,7 @@ const withPWA = withPWAInit({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "standalone",
+  output: "standalone",
   reactStrictMode: true,
   logging: {
     fetches: {
