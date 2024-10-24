72f00ac8b317da05bd011eb4dce0bd8dd3e4f0c3
hamster1963 <1410514192@qq.com>
2024-10-24 21:30:48 +0800

    fix: route

--- app/api/server/route.ts
+++ app/api/server/route.ts
@@ -27,32 +27,3 @@ export const GET = auth(async function GET(req) {
     return NextResponse.json({ error: message }, { status: statusCode });
   }
 });
-import { auth } from "@/auth";
-import getEnv from "@/lib/env-entry";
-import { GetNezhaData } from "@/lib/serverFetch";
-import { redirect } from "next/navigation";
-import { NextResponse } from "next/server";
-
-export const dynamic = "force-dynamic";
-
-export const runtime = "edge";
-
-
-
-export const GET = auth(async function GET(req) {
-  if (!req.auth && getEnv("SitePassword")) {
-    redirect("/");
-  }
-
-  try {
-    const data = await GetNezhaData();
-    return NextResponse.json(data, { status: 200 });
-  } catch (error) {
-    console.error("Error in GET handler:", error);
-    // @ts-ignore
-    const statusCode = error.statusCode || 500;
-    // @ts-ignore
-    const message = error.message || "Internal Server Error";
-    return NextResponse.json({ error: message }, { status: statusCode });
-  }
-});
