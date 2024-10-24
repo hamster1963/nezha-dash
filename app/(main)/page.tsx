260bbb508104a0d709bd3d5059c79c78edecf319
hamster1963 <1410514192@qq.com>
2024-10-24 21:42:23 +0800

    fix: remove unstable_setRequestLocale

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,14 +1,7 @@
 import ServerList from "@/components/ServerList";
 import ServerOverview from "@/components/ServerOverview";
-import { unstable_setRequestLocale } from "next-intl/server";
 
-export default function Home({
-  params: { locale },
-}: {
-  params: { locale: string };
-}) {
-  unstable_setRequestLocale(locale);
-
+export default function Home() {
   return (
     <div className="mx-auto grid w-full max-w-5xl gap-4 md:gap-6">
       <ServerOverview />
