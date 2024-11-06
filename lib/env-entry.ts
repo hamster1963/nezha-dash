9854df5199f2417b80990e5a8f6394ba888e461f
仓鼠 <71394853+hamster1963@users.noreply.github.com>
2024-11-06 18:40:58 +0800

    fix： vercel env

--- lib/env-entry.ts
+++ lib/env-entry.ts
@@ -1,12 +1,8 @@
 import { env } from "next-runtime-env";
 
 export default function getEnv(key: string) {
-  if (process.env.VERCEL) {
-    return process.env[key];
-  } else {
     if (key.startsWith("NEXT_PUBLIC_")) {
       return env(key);
     }
     return process.env[key];
-  }
 }
