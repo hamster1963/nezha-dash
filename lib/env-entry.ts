913a9051004bd33bf955f6f794c433e99336fa45
hamster1963 <1410514192@qq.com>
2024-11-06 15:57:17 +0800

    perf(serverless): fallback to static env

--- lib/env-entry.ts
+++ lib/env-entry.ts
@@ -1,8 +1,12 @@
 import { env } from "next-runtime-env";
 
 export default function getEnv(key: string) {
-  if (key.startsWith("NEXT_PUBLIC_")) {
-    return env(key);
+  if (process.env.VERCEL || process.env.CF_PAGES) {
+    return process.env[key];
+  } else {
+    if (key.startsWith("NEXT_PUBLIC_")) {
+      return env(key);
+    }
+    return process.env[key];
   }
-  return process.env[key];
 }
