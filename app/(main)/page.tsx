6ed640e9e48e5d279f71f550e751aea286e5f757
hamster1963 <1410514192@qq.com>
2024-11-19 21:34:15 +0800

    feat: add global map

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,13 +1,19 @@
 import ServerList from "@/components/ServerList";
 import ServerOverview from "@/components/ServerOverview";
-import Global from "./ClientComponents/Global";
 
-export default function Home() {
+import ServerGlobal from "./ClientComponents/Global";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const global = (await searchParams).global;
   return (
     <div className="mx-auto grid w-full max-w-5xl gap-4 md:gap-6">
       <ServerOverview />
-      {/* <ServerList /> */}
-      <Global />
+      {!global && <ServerList />}
+      {global && <ServerGlobal />}
     </div>
   );
 }
