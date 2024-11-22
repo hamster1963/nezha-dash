ce82c67f88cd98ca8fe2cd6fd0b40d2c50e1f60f
hamster1963 <1410514192@qq.com>
2024-11-23 00:12:08 +0800

    style(global): colorful button

--- components/GlobalBackButton.tsx
+++ components/GlobalBackButton.tsx
@@ -23,9 +23,9 @@ export default function GlobalBackButton() {
       onClick={() => {
         router.push(`/`);
       }}
-      className="rounded-[50px] cursor-pointer w-fit bg-stone-100 p-[10px] transition-all hover:bg-stone-200 dark:hover:bg-stone-700 dark:bg-stone-800"
+      className="rounded-[50px] mt-[1px] w-fit text-white cursor-pointer [text-shadow:_0_1px_0_rgb(0_0_0_/_20%)] bg-green-600 hover:bg-green-500 p-[10px] transition-all shadow-[inset_0_1px_0_rgba(255,255,255,0.2)] hover:shadow-[inset_0_1px_0_rgba(0,0,0,0.2)] "
     >
-      <ServerStackIcon className="size-4" />
+      <ServerStackIcon className="size-[13px]" />
     </button>
   );
 }
