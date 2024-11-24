8f673497ba86c0ac2a16267b3f8e4439e0f601fd
hamster1963 <1410514192@qq.com>
2024-11-24 22:12:03 +0800

    style: card hover color

--- components/ServerCard.tsx
+++ components/ServerCard.tsx
@@ -37,7 +37,7 @@ export default function ServerCard({
     <Link onClick={saveSession} href={`/${id}`} prefetch={true}>
       <Card
         className={cn(
-          "flex flex-col items-center justify-start gap-3 p-3 md:px-5 cursor-pointer",
+          "flex flex-col items-center justify-start gap-3 p-3 md:px-5 cursor-pointer hover:bg-accent/50 transition-colors",
           {
             "flex-col": fixedTopServerName,
             "lg:flex-row": !fixedTopServerName,
