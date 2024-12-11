8f38cf94d8decb2f31299f11c4474a8994880b08
hamster1963 <1410514192@qq.com>
2024-12-12 00:45:04 +0800

    fix: get file

--- lib/GetIPInfo.ts
+++ lib/GetIPInfo.ts
@@ -2,6 +2,7 @@
 
 import maxmind, { CityResponse, AsnResponse } from 'maxmind';
 import { GetServerIP } from './serverFetch';
+import path from 'path';
 
 type IPInfo = {
     city: CityResponse;
@@ -9,11 +10,14 @@ type IPInfo = {
 }
 
 export default async function GetIPInfo({ server_id }: { server_id: string }): Promise<IPInfo> {
-
     const ip = await GetServerIP({ server_id: Number(server_id) })
+    
+    // 使用 path.join 获取正确的文件路径
+    const cityDbPath = path.join(process.cwd(), 'lib', 'GeoLite2-City.mmdb');
+    const asnDbPath = path.join(process.cwd(), 'lib', 'GeoLite2-ASN.mmdb');
 
-    const cityLookup = await maxmind.open<CityResponse>('./lib/GeoLite2-City.mmdb')
-    const asnLookup = await maxmind.open<AsnResponse>('./lib/GeoLite2-ASN.mmdb')
+    const cityLookup = await maxmind.open<CityResponse>(cityDbPath)
+    const asnLookup = await maxmind.open<AsnResponse>(asnDbPath)
 
     return {
         city: cityLookup.get(ip) as CityResponse,
