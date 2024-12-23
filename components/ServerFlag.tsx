9ff5913ffb28e189395c1aa87705478a47c452e1
hamster1963 <1410514192@qq.com>
2024-12-23 11:18:14 +0800

    fix: remove flag transform

--- components/ServerFlag.tsx
+++ components/ServerFlag.tsx
@@ -40,10 +40,6 @@ export default function ServerFlag({
 
   if (!country_code) return null
 
-  if (supportsEmojiFlags && country_code.toLowerCase() === "tw") {
-    country_code = "cn"
-  }
-
   return (
     <span className={cn("text-[12px] text-muted-foreground", className)}>
       {useSvgFlag || !supportsEmojiFlags ? (
