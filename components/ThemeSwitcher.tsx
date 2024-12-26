a9a25f821879cae8ec2d17a7a1bae5452abeb40b
hamster1963 <hamster1963@users.noreply.github.com>
2024-12-26 06:41:34 +0000

    chore: auto-fix linting and formatting issues

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -13,7 +13,6 @@ import { Moon, Sun } from "lucide-react"
 import { useTranslations } from "next-intl"
 import { useTheme } from "next-themes"
 
-
 export function ModeToggle() {
   const { setTheme, theme } = useTheme()
   const t = useTranslations("ThemeSwitcher")
