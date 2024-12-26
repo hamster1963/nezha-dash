193164566a63c2345d11c5f746eee8a30b8c8f70
hamster1963 <1410514192@qq.com>
2024-12-26 14:40:55 +0800

    test: biome

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -13,6 +13,7 @@ import { Moon, Sun } from "lucide-react"
 import { useTranslations } from "next-intl"
 import { useTheme } from "next-themes"
 
+
 export function ModeToggle() {
   const { setTheme, theme } = useTheme()
   const t = useTranslations("ThemeSwitcher")
