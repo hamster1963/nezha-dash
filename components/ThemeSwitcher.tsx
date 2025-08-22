55085aaad6ac9bd3a2a952df91bcc6bc9bc72a7f
hamster1963 <1410514192@qq.com>
2025-08-22 23:22:01 +0800

    fix: reorder imports for consistency in ThemeSwitcher component

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { CheckCircleIcon, Moon, Sun } from "lucide-react"
+import { CheckCircleIcon } from "@heroicons/react/20/solid"
+import { Moon, Sun } from "lucide-react"
 import { useTranslations } from "next-intl"
 import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
